9879243ed45fce4c877564901a93495e11027e17
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-09-09 16:34:22 -0700

    completed

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+    let min = array[0]
+    let minIdx = 0
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i]
+            minIdx = i
+        }
+    }
+    array.splice(minIdx, 1)
+    return min
 }
 
 function selectionSort(array){
-
+    const sorted = []
+    let newMin
+    while (array.length) {
+        newMin = findMinAndRemove(array)
+        sorted.push(newMin)
+    }
+    return sorted
 }
